aa83ce35fc7b96f545d1c04247b2c5408426532a
Ekaterina <tuc5235@gmail.com>
2023-02-24 11:27:02 +0300

    did the task 2

--- sem1/Zadanie2/Program.cs
+++ sem1/Zadanie2/Program.cs
@@ -3,4 +3,21 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.Write("enter first number ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("enter socond number ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("enter third number ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if(a>max) max = a;
+if(b>max) max = b;
+if(c>max) max = c;
+
+Console.Write("max =  ");
+Console.WriteLine(max);
+
+
+
 
