9b5d630674ed98414c7338ba9557b5bb78daf9e8
Ekaterina <tuc5235@gmail.com>
2023-02-24 12:51:40 +0300

    even or odd number_task

---
+++ sem1/Zadanie3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("enter number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0){
+    Console.WriteLine("even number");
+}
+else Console.WriteLine("odd number");
+    
