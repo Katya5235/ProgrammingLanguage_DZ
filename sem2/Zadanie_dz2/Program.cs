b96bcf29e7ba551ecf36735a17f1d19f2325d70f
Ekaterina <tuc5235@gmail.com>
2023-02-28 12:37:20 +0300

    создала новую ветку черновик

---
+++ sem2/Zadanie_dz2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine(" Enter number ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100) Console.WriteLine("no third number ");
+
+if (number >100){
+    int thirdnumber = number % 10;
+
+    Console.WriteLine(thirdnumber);
+}
+
