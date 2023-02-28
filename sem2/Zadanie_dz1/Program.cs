d4fb869a52bb93ca12ea77d5e8f3aaccc95c6440
Ekaterina <tuc5235@gmail.com>
2023-02-28 10:41:32 +0300

    first homework assignment

---
+++ sem2/Zadanie_dz1/Program.cs
@@ -0,0 +1,9 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Enter a three-digit number ");
+int number = Convert.ToInt32(Console.ReadLine()); // 589
+int secondnumber = number / 10 % 10;
+Console.WriteLine(secondnumber);
