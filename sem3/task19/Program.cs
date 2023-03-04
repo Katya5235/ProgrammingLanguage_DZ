012c09df986f9ab4db700a72f3b60aecb5b82db2
Ekaterina <tuc5235@gmail.com>
2023-03-04 14:15:14 +0300

    полиндром или нет

--- sem3/task19/Program.cs
+++ sem3/task19/Program.cs
@@ -3,3 +3,16 @@
 // 12821 -> да
 // 23432 -> да
 
+Console.WriteLine("enter five-digit number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int pol = 0;
+int num = number;
+
+while (number > 0)
+{
+    pol = pol * 10 + number % 10;
+    number /= 10;
+}
+if (pol == num) Console.WriteLine("polyndrom");
+else Console.WriteLine("not a polyndrome");
