97da7e55e378c6803df520d0612abe4fe39ad447
Ekaterina <tuc5235@gmail.com>
2023-03-04 15:50:39 +0300

    таблица кубов чисел

---
+++ sem3/task23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i < number+1; i++)
+{
+    Console.WriteLine(Math.Pow(i, 3));
+}
