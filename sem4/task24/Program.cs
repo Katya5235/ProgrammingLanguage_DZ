8af7ee3fba6818a07bf0939e0a09101e98feb27b
Ekaterina <tuc5235@gmail.com>
2023-03-08 11:02:38 +0300

    summa chisel ot 1 do A

---
+++ sem4/task24/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int ReadIntNumber(string mesageToUser)
+{
+    Console.WriteLine(mesageToUser);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int SumFrom1ToA(int a)
+{
+    int sum = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
+}
+
+int number = ReadIntNumber("enter variable a ");
+int result = SumFrom1ToA(number);
+Console.WriteLine($"summa digits from 1 to {number} = {result}");
+
+
+// int b = ReadIntNumber("enter variable b ");
+// int c = ReadIntNumber("enter variable c ");
