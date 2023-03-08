6103dc5381ec83a02bdea2de7f017a2b6b9135c1
Ekaterina <tuc5235@gmail.com>
2023-03-08 19:43:26 +0300

    task25

---
+++ sem4/task25/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int ReadIntNumber()
+{
+    return Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Введите число A и B ");
+
+int A = ReadIntNumber();
+int B = ReadIntNumber();
+
+
+
+// int count = 0;
+// if (A >B)
+// {
+//     count = A + 1 - B;
+// }
+// else count = B + 1 - A;
+
+// Решение: k=329-1-7=321.
+// Console.WriteLine(count);
+// int bot = A;
+int a = A;
+int count = B;
+int sum = 1;
+
+    sum*=a;
+
+
+Console.WriteLine(sum);
