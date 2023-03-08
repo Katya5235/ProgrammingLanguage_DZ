af9dd6fed8300ba7b3a523b3df623c4a3c9c0ecc
Ekaterina <tuc5235@gmail.com>
2023-03-08 16:40:43 +0300

    Product of numbers

---
+++ sem4/task28/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Enter number ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number;
+long pro = 1;
+for (int i = 1; i <= a; i++)
+{
+    pro *= i;
+    
+}
+Console.WriteLine($"Product of a number {a} - {pro}");
