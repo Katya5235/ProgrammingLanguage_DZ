6a6417b27d500812d4f24c7a4031dd4a56920b13
Ekaterina <tuc5235@gmail.com>
2023-03-09 16:40:13 +0300

    Ввод массива с клавиатуры

--- sem4/task29/Program.cs
+++ sem4/task29/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран. 
+// Данные вводятся с консоли пользователем
+
+
+
+Console.WriteLine("Введите кол-во элементов массива  ");
+int elements = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[elements];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите элемент массива под индексом {i} -  ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Вывод массива ");
+
+for (int i = 0; i < array.Length; i++)
+{
+Console.WriteLine(array[i]);
+}
+
+
+
+
+
+
+
