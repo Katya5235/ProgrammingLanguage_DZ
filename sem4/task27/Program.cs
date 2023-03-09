9f7326751ff7c85dc2bdb8a23201bc452cde6708
Ekaterina <tuc5235@gmail.com>
2023-03-09 10:19:36 +0300

    Нахождение суммы цифр в числе

--- sem4/task27/Program.cs
+++ sem4/task27/Program.cs
@@ -4,9 +4,43 @@
 // 82 -> 10
 // 9012 -> 12
 
-int number = Convert.ToInt32(Console.ReadLine());
+int EnterNumber(string mesage)
+{
+    Console.WriteLine(mesage);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int SizeNumber(int a)
+{
+    int i = 0;
+    while (a > 0)
+    {
+        i++;
+        a /= 10;
+    }
+    return i;
+}
+
+int Sumdigit(int b)
+{
+    int res = 0;
+    int s = 0;
+    while (b > 0)
+    {
+        s = b % 10;
+        res = res + s;
+        b /= 10;
+    }
+    return res;
+}
 
 
+int num = EnterNumber(" Enter your favorite number  ");
+int size = SizeNumber(num);
+Console.WriteLine($" number of digits in {num} = {size}");
+int sdig = Sumdigit(num);
+Console.WriteLine($" Sum of digits in {num} = {sdig}");
 
 
 
