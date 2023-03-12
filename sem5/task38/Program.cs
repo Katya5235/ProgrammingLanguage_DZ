30d902edad3f8be3131a58ef770542763b80d628
Ekaterina <tuc5235@gmail.com>
2023-03-12 10:18:11 +0300

    Разница между минимальным и максимальным значениями

---
+++ sem5/task38/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 38: Задайте массив натуральных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] array = new int[] { 55, 68, 34, 6, 97, 81, 13 };
+
+int SearchMaxDigit(int[] a)
+{
+
+    int max = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+
+    }
+    return max;
+}
+
+int MaxDigit = SearchMaxDigit(array);
+Console.WriteLine($" Максимальное число = {MaxDigit}");
+
+int SearchMinDigit(int[] b)
+{
+    int min = MaxDigit;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+int MinDigit = SearchMinDigit(array);
+Console.WriteLine($" Минимальное число = {MinDigit}");
+Console.WriteLine($" Разница между максимальным и минимальнымзначением  = {MaxDigit - 1 - MinDigit}");
