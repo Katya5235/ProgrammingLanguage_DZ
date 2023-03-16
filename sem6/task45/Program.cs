e1e2fe06de7010c7b583e65cc9c730ee9b475cdb
Ekaterina <tuc5235@gmail.com>
2023-03-16 11:49:18 +0300

    копирование массива

---
+++ sem6/task45/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 45: Напишите программу,
+//которая будет создавать копию массива с помощью поэлементного копирования.
+
+int[] GetArray()
+{
+    int[] array = new int[9];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(1, 51);
+    }
+    return array;
+
+}
+int[] myarray = GetArray();
+int[] copyarray = new int[9];
+for (int i = 0; i < myarray.Length; i++)
+{
+copyarray[i] = myarray[i];
+}
+
+myarray[2] = 2000;
+
+Console.WriteLine($" Array - {string.Join(", ", myarray)}");
+Console.WriteLine($" Array copy - {string.Join(", ", copyarray)}");
+
+
+
