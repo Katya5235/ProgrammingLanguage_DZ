2d66e6f15326615dc60b6d03aed5913d9a4a01a1
Ekaterina <tuc5235@gmail.com>
2023-03-16 10:25:55 +0300

    Переворачивание массива и заполнение массива чрезе консоль

---
+++ sem6/task39/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+
+int[] FillArray()
+{
+    Console.WriteLine("Введите кол-во элементов массива  ");
+    int elements = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[elements];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите элемент массива под индексом {i} -  ");
+        array[i] = int.Parse(Console.ReadLine());
+
+    }
+    Console.Write($"Вывод массива - {string.Join(", ", (array))}");
+    return array;
+}
+
+
+void Reverse(int[]a)
+{
+    for (int i = 0; i < a.Length / 2; i++)
+    {
+        int temp = a[i];
+        a[i] = a[a.Length - i - 1];
+        a[a.Length - i - 1] = temp;
+    }
+Console.Write($"Массив перевёрнутый {string.Join(", ",(a))}");
+}
+
+int [] ser = FillArray();
+Console.WriteLine();
+Reverse(ser);
+
+
+
+
