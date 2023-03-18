395813b97485ea15434aef993ae05d615987bc9b
Ekaterina <tuc5235@gmail.com>
2023-03-18 21:41:05 +0300

    двумерный массив заполненый случайными вещественными числами

---
+++ sem7/task47/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный 
+// случайными вещественными числами.
+// ​ m = 3, n = 4.
+// ​ 0,5 7 -2 -0,2
+//  1 -3,3 8 -9,9
+//  8 7,8 -7,1 9
+
+
+//   double ArrayDimension
+
+Console.WriteLine("Введие размерность строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размерность столбцов массива: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[rows, colums];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(double[,] ar)
+{
+    for (int i = 0; i < ar.GetLength(0); i++)
+    {
+        for (int j = 0; j < ar.GetLength(1); j++)
+        {
+            ar[i, j] = Convert.ToDouble(new Random().Next(1, 16)) / 10;
+
+        }
+    }
+}
+
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        
+        Console.WriteLine("");
+    }
+}
