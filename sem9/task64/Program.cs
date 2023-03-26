8f15327cbb9352d1132f1856788973b3b5f627d5
Ekaterina <tuc5235@gmail.com>
2023-03-26 16:33:40 +0300

    числа по убыванию от N до 1

---
+++ sem9/task64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int number = 6;
+PrintDescendingNumbers(number);
+void PrintDescendingNumbers (int n)
+{
+    if(n==0) return;
+    Console.WriteLine(n+"");
+    PrintDescendingNumbers(n-1);
+    
+}
